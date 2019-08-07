8cad8defb9bbf2e5dbcd1c3b677aca9912094bc4
Bruno De Angelis <41843151+brunodeangelis@users.noreply.github.com>
2019-08-07 11:52:38 -0300

    scaffold

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="font-sans">
+    <nuxt />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-5xl text-red-500">hola</h1>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+</style>
