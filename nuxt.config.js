f313328e8994aabc94a7bcfd55b36765ec18c0f7
Bruno De Angelis <41843151+brunodeangelis@users.noreply.github.com>
2019-08-08 00:42:21 -0300

    fixed localStorage plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  // plugins: [{ src: "~/plugins/localStorage.js", ssr: false }],
+  plugins: [{ src: "~/plugins/localStorage.js", ssr: false }],
 
   /*
    ** Nuxt.js modules
